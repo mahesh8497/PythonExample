58e9f828a1cf3f4d3642fff3066d524efb7ce2b2
mahesh8497 <maheshmtakale97@gmail.com>
2024-03-05 20:07:25 +0530

    areaTriangle

---
+++ PythonExample/example4.py
@@ -0,0 +1,7 @@
+#Area of triangle
+
+base = float (input("Enter the length of the base of triangle: "))
+height = float(input("Enter the length of the height of triangle: "))
+area = 0.5*base*height
+print(f"Area of given Triangle is: {area}")
+
