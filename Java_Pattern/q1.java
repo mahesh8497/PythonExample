ffdc3acda367e0292b50673d220c7a972b5a33cb
mahesh8497 <maheshmtakale97@gmail.com>
2024-04-01 21:51:24 +0530

    Star Pattern

---
+++ Java_Pattern/q1.java
@@ -0,0 +1,12 @@
+package Pattern;
+
+public class q1 {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++){  //i=row
+            for (int j=0;j<5;j++){  //j=column
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Java_Pattern/q2.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class q2 {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            for (int j=0;j<5;j++){
+                if(j+i<=4)
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
